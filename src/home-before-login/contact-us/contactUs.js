8dccfbbba8bc2f7e425c4808ebe585173bc185dd
rakean <rakean321@gmail.com>
2024-11-03 23:10:44 +0700

    menambahkan page contact us final

--- src/home-before-login/contact-us/contactUs.js
+++ src/home-before-login/contact-us/contactUs.js
@@ -1,24 +1,52 @@
 window.onscroll = function () {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    const navbar = document.getElementById("navbar");
-  
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 90) {
-      navbar.style.backgroundColor = "rgba(255, 255, 255, 0.28);";
-      //navbar.style.backgroundColor = "rgba(4, 41, 58, 1)";
-      navbar.style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.1)";
-      navbar.style.backdropFilter = "blur(7.9px)";
-      navbar.style.webkitBackdropFilter = "blur(7.9px)";
-      navbar.style.border = "1px solid rgba(255, 255, 255, 1)";
-    } else {
-      navbar.style.backgroundColor = "transparent";
-      navbar.style.boxShadow = "";
-      navbar.style.backdropFilter = "";
-      navbar.style.webkitBackdropFilter = "";
-      navbar.style.border = "";
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const navbar = document.getElementById("navbar");
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 90) {
+    navbar.style.backgroundColor = "rgba(255, 255, 255, 0.28);";
+    //navbar.style.backgroundColor = "rgba(4, 41, 58, 1)";
+    navbar.style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.1)";
+    navbar.style.backdropFilter = "blur(7.9px)";
+    navbar.style.webkitBackdropFilter = "blur(7.9px)";
+    navbar.style.border = "1px solid rgba(255, 255, 255, 1)";
+  } else {
+    navbar.style.backgroundColor = "transparent";
+    navbar.style.boxShadow = "";
+    navbar.style.backdropFilter = "";
+    navbar.style.webkitBackdropFilter = "";
+    navbar.style.border = "";
+  }
+}
+const swiper = new Swiper('.slider-wrapper', {
+
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
     }
   }
-  
-  
+
+});
